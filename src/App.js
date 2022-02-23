28b45a5a7b1b67a1e62173425d10772e0f3e8de7
Trey Kemp <kemptrey@outlook.com>
2022-02-22 19:10:27 -0700

    semantic not working.. need to debug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import { Container, Header } from "semantic-ui-react";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>React Contact List</h1>
-      </div>
+      <Container>
+        <Header as="h1">React Contact List</Header>
+      </Container>
     );
   }
 }
