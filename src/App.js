383c97ffc8e4be7ce15c960e781b8b4f111848c2
Trey Kemp <kemptrey@outlook.com>
2022-02-23 17:02:34 -0700

    added contacts;

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import { Container, Header } from "semantic-ui-react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    contacts: [
+      { id: 1, firstName: 'Jeffery', phone: '801-121-5656',},
+      { id: 2, firstName: 'George', phone: '801-121-5656',},
+      { id: 3, firstName: 'Ringo', phone: '801-121-5656',},
+    ],
+  }
+
+
+
+
   render() {
     return (
-      <Container>
-        <Header as="h1">React Contact List</Header>
-      </Container>
+      <div>
+        <h1>React Contact List</h1>
+      </div>
     );
   }
 }
